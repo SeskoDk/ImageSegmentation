4538d3aa52c4bac77887383194ac4a76d8f8bee4
Sesko <Sesko.DK@gmail.com>
2023-05-06 17:53:33 +0200

    Iteration with KxK receptive field over image implemented.

--- src/unsharpMasking.py
+++ src/unsharpMasking.py
@@ -12,14 +12,16 @@ PADDING_SIZE = KERNEL_SIZE // 2
 
 # blurring kernel
 blurr_k = np.ones(shape=(KERNEL_SIZE, KERNEL_SIZE), dtype=TYPE) / KERNEL_SIZE ** 2
-
 padded_image = np.pad(array=image, pad_width=PADDING_SIZE)
-
 height, width = padded_image.shape
 
-# plt.imshow(image, cmap='gray')
-# plt.show()
+K = 3
+P = K // 2
 
-A = np.arange(1, 21).reshape(4, 5)
-print(A)
-
+A = np.arange(0, 20).reshape(5, 4)
+B = np.pad(A, pad_width=P)
+H, W = A.shape
+for row_i in range(H):
+    for col_j in range(W):
+        sub_arr = B[row_i: K + row_i, col_j: K + col_j]
+        print(sub_arr)
